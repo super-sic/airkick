f07287251e0011e556cfd1fc81a2d5fa4488d988
theacehorsemen <65445074+theacehorsemen@users.noreply.github.com>
2020-10-19 22:18:57 +0530

    commit

---
+++ client/src/main.js
@@ -0,0 +1,17 @@
+// import "@/css/font-roboto.css";
+import "@/css/font-montserrat.css";
+import Vue from 'vue'
+import App from '@/App.vue'
+import { vuetify, i18n } from "@/plugins/vuetify";
+import router from '@/router/index'
+import store from "@/store";
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  i18n,
+  render: h => h(App)
+}).$mount('#app')
